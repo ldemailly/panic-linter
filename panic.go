a34e34869c4e3549d1267c29869c921b7b03d021
Laurent Demailly <ldemailly@gmail.com>
2024-10-08 10:01:15 -0700

    Move the Analyser to a package outside of main so it can be used in golanglint-ci

--- panic.go
+++ panic.go
@@ -1,64 +1,10 @@
 package main
 
 import (
-	"go/ast"
-	"strings"
-
-	"golang.org/x/tools/go/analysis"
+	"github.com/ldemailly/panic-linter/analyser"
 	"golang.org/x/tools/go/analysis/singlechecker"
-
-	"fortio.org/sets"
 )
 
 func main() {
-	singlechecker.Main(analyzer)
-}
-
-var analyzer = &analysis.Analyzer{
-	Name: "paniccheck",
-	Doc:  "reports panic() calls without comments",
-	Run:  run,
-}
-
-func run(pass *analysis.Pass) (interface{}, error) {
-	for _, f := range pass.Files {
-		// Create a map of lines that have comments
-		commentLines := sets.New[int]()
-		for _, commentGroup := range f.Comments {
-			for _, comment := range commentGroup.List {
-				if strings.HasPrefix(comment.Text, "/* want") { // skipping analysistests special comments
-					continue
-				}
-				commentPos := pass.Fset.Position(comment.Pos())
-				commentLines.Add(commentPos.Line)
-			}
-		}
-		ast.Inspect(f, func(node ast.Node) bool {
-			return CheckPanicCalls(pass, f, node, commentLines)
-		})
-	}
-	return nil, nil
-}
-
-type Result struct {
-	IssueLines []int // Lines with issues, empty if no issues.
-}
-
-func CheckPanicCalls(pass *analysis.Pass, file *ast.File, node ast.Node, commentLines sets.Set[int]) bool {
-	// Check for a panic call
-	callExpr, ok := node.(*ast.CallExpr)
-	if !ok {
-		return true
-	}
-	funIdent, ok := callExpr.Fun.(*ast.Ident)
-	if !ok || funIdent.Name != "panic" {
-		return true
-	}
-
-	startPos := pass.Fset.Position(node.Pos())
-	// Check if the line has a comment
-	if !commentLines.Has(startPos.Line) {
-		pass.Reportf(node.Pos(), "panic call without same line comment justifying it")
-	}
-	return true
+	singlechecker.Main(analyser.Analyser)
 }
--- panic_test.go
+++ panic_test.go
@@ -3,10 +3,11 @@ package main
 import (
 	"testing"
 
+	"github.com/ldemailly/panic-linter/analyser"
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
 func TestAll(t *testing.T) {
-	_ = analysistest.Run(t, "./testdata", analyzer, "./...")
+	_ = analysistest.Run(t, "./testdata", analyser.Analyser, "./...")
 
 }
